ebb6036dfece27c0cde495b5b5cc26c9f54618c1
Sophie Ostmeier <ostmeiersophie@gmail.com>
2022-12-17 16:01:32 +0000

    fixed something, not sure what

--- nnunet/adaptable_platform/download_data.sh
+++ nnunet/adaptable_platform/download_data.sh
@@ -12,5 +12,5 @@ jeremy_folder=$nnUNet_preprocessed/Task127_experts_227/nnUNetData_plans_v2.1_sta
 mkdir -p $jeremy_folder
 gcloud storage cp gs://task127_experts_227_train_jeremy/* $jeremy_folder
 
-plan_folder=$nnUNet_preprocessed/Task127_experts_227/
+plan_folder=/home/ostmeiersophie/NCCTfolders/nnUNet_preprocessed/Task127_experts_227/
 gcloud storage cp gs://task127_experts_227/* $plan_folder
