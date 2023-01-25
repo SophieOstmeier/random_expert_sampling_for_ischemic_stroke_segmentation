e19a4c340bdea58665594bb0e09dcafba47f9476
SophieOstmeier <ostmeiersophie@gmail.com>
2023-01-25 12:45:19 -0800

    debugging

--- nnunet/training/network_training/nnUNetTrainerV2_random_data_loader_3rater.py
+++ nnunet/training/network_training/nnUNetTrainerV2_random_data_loader_3rater.py
@@ -308,8 +308,10 @@ class nnUNetTrainerV2_random_data_loader_3rater(nnUNetTrainerV2):
         gt_nifti_folder = join(self.output_folder_base, "gt_niftis")
         maybe_mkdir_p(gt_nifti_folder)
 
-        split_data = join(self.folder_with_preprocessed_data.rsplit("/")[0], "splits_final.pkl")
-        print(self.folder_with_preprocessed_data.rsplit("/")[0])
+
+        print(self.folder_with_preprocessed_data.rsplit("/",1)[0])
+        base = self.folder_with_preprocessed_data.rsplit("/",1)[0]
+        split_data = join(base, "splits_final.pkl")
         validation_list = self.gt_niftis_validation_list(split_data)
 
         for f in subfiles(self.gt_niftis_folder_random, suffix=".nii.gz"):
