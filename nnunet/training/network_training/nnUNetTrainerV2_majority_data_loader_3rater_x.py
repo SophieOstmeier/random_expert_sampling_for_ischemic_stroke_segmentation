883b47813fe07a23b2b6f3b241c6c8f8ad0ad98a
SophieOstmeier <ostmeiersophie@gmail.com>
2023-01-27 14:47:52 -0800

    debugging

--- nnunet/training/network_training/nnUNetTrainerV2_majority_data_loader_3rater_x.py
+++ nnunet/training/network_training/nnUNetTrainerV2_majority_data_loader_3rater_x.py
@@ -31,7 +31,7 @@ import glob
 from nnunet.network_architecture.neural_network import SegmentationNetwork
 import torch
 
-class nnUNetTrainerV2_majority_data_loader_3rater(nnUNetTrainerV2):
+class nnUNetTrainerV2_majority_data_loader_3rater_x(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
